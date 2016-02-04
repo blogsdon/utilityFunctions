eb4be78c89eea717412924a9034f440c6af0e7c8
Benjamin Logsdon <aeberhart@gmail.com>
2016-02-03 16:47:48 -0800

    matrix apply function

---
+++ R/matrixApply.R
@@ -0,0 +1,7 @@
+matrixApply <- function(FUN,MARGIN,X,Y){
+  require(dplyr)
+  internal <- function(X,Y,FUN,MARGIN){
+    return(Y%>% apply(MARGIN,FUN,X))
+  }
+  return(X %>% apply(MARGIN,internal,Y,FUN,MARGIN))
+}
