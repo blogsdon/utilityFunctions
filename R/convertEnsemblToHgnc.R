e1f6e25465cced263b86b980dfcd9dd75333acb9
Ben Logsdon <blogsdon@c02vp283hv2q.fhcrc.org>
2018-01-24 12:25:04 -0800

    updates

--- R/convertEnsemblToHgnc.R
+++ R/convertEnsemblToHgnc.R
@@ -3,7 +3,7 @@ convertEnsemblToHgnc <- function(ensemblIds){
     library(biomaRt)
     ensembl=biomaRt::useMart('ENSEMBL_MART_ENSEMBL',
                            dataset = 'hsapiens_gene_ensembl',
-                           host='www.ensembl.org')
+                           host='uswest.ensembl.org')
 
     genes<-getBM(attributes = c('ensembl_gene_id','external_gene_name'),
                filters='ensembl_gene_id',
